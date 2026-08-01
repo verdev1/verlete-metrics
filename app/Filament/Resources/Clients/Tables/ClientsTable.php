0d8148450f32f81dddf12f3a313b94be7bbf33cd
Matt <mattyoung1985@gmail.com>
2026-08-02 08:49:22 +1000

    Removed test field from list view

--- app/Filament/Resources/Clients/Tables/ClientsTable.php
+++ app/Filament/Resources/Clients/Tables/ClientsTable.php
@@ -19,10 +19,6 @@ class ClientsTable
                     ->label('Active')
                     ->boolean()
                     ->sortable(),
-                IconColumn::make('is_test')
-                    ->label('Test')
-                    ->boolean()
-                    ->sortable(),
                 TextColumn::make('name')
                     ->searchable()
                     ->sortable()
