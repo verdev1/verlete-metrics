9b7613521233db2dafca739db84e8e40b2a77281
Matt <mattyoung1985@gmail.com>
2026-08-01 17:15:12 +1000

    Set test field default to false

--- database/migrations/2026_08_01_070704_add_is_test_to_clients_table.php
+++ database/migrations/2026_08_01_070704_add_is_test_to_clients_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::table('clients', function (Blueprint $table) {
             $table->boolean('is_test')
-                ->default(true)
+                ->default(false)
                 ->after('name');
         });
     }
